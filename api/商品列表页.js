04709022c85ed8febb05aa94fa4f7eb913d02d8c
Xuhengfeng <564297479@qq.com>
2017-10-15 13:25:30 +0800

    update

---
+++ api/商品列表页.js
@@ -0,0 +1,262 @@
+'use strict';
+
+let data = [
+	
+		    {
+		        "isAld": "true", 
+		        "alds": [
+		            {
+		                "img": "//img.alicdn.com/tps/i4/TB1pa4rMXXXXXbWaXXXee.i.VXX-346-402.png", 
+		                "tce_rule_count": "1", 
+		                "link": "https://pages.tmall.com/wow/huanxin/act/refreshment-pc"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/tps/i3/TB13LNGMXXXXXcBXpXXlkjU.VXX-346-200.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://pages.tmall.com/wow/huanxin/act/mustbuy"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/tps/i2/TB1TaNBMXXXXXcBXFXXlkjU.VXX-346-200.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://shouji.tmall.com/"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/tps/i4/TB1_1BLMXXXXXb3XXXXlkjU.VXX-346-200.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://ku.tmall.com/"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/tps/i3/TB1OK4CMXXXXXboXFXXlkjU.VXX-346-200.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://miao.m.tmall.com/"
+		            }
+		        ], 
+		        "isTanx": "false", 
+		        "share": "false", 
+		        "id": "201602246", 
+		        "open": "false", 
+		        "local": "false"
+		    }, 
+		    {
+		        "isAld": "false", 
+		        "isTanx": "true", 
+		        "share": "false", 
+		        "id": "mm_12852562_1778064_95126332", 
+		        "tanxs": [
+		            {
+		                "isope": "false", 
+		                "tce_rule_count": "1", 
+		                "id": "mm_12852562_1778064_48432768"
+		            }, 
+		            {
+		                "isope": "false", 
+		                "tce_rule_count": "1", 
+		                "id": "mm_12852562_1778064_48458242"
+		            }
+		        ], 
+		        "open": "false", 
+		        "local": "false"
+		    }, 
+		    {
+		        "isAld": "true", 
+		        "len": "4", 
+		        "alds": [
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i3/92/TB2J9XZc71M.eBjSZFFXXc3vVXa_!!92-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://pages.tmall.com/wow/chaoshi/act/cszs-11"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i3/35/TB2y5X0cxuI.eBjy0FdXXXgbVXa_!!35-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://nanzhuang.tmall.com"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i3/193/TB2Eq69cV5N.eBjSZFmXXboSXXa_!!193-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://nvxie.tmall.com"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i4/62/TB2sF9hcrmI.eBjy1zjXXaq5VXa_!!62-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://nvzhuang.tmall.com/"
+		            }
+		        ], 
+		        "start": "0", 
+		        "isTanx": "false", 
+		        "share": "true", 
+		        "id": "07085", 
+		        "open": "false", 
+		        "local": "false"
+		    }, 
+		    {
+		        "isAld": "true", 
+		        "len": "4", 
+		        "alds": [
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i2/146/TB2vB5Gc2SM.eBjSZFNXXbgYpXa_!!146-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://bag.tmall.com"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i4/155/TB2yC4bcrWJ.eBjSspdXXXiXFXa_!!155-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://3c.tmall.com"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i3/143/TB207eEc.OO.eBjSZFLXXcxmXXa_!!143-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://watch.tmall.com/"
+		            }, 
+		            {
+		                "img": "https://gw.alicdn.com/imgextra/i4/57/TB23Ej8c71M.eBjSZFOXXc0rFXa_!!57-0-yamato.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "https://mei.tmall.com/"
+		            }
+		        ], 
+		        "start": "4", 
+		        "isTanx": "false", 
+		        "share": "true", 
+		        "id": "07085", 
+		        "open": "false", 
+		        "local": "false"
+		    }, 
+		    {
+		        "isAld": "true", 
+		        "alds": [
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/TB1WgndFVXXXXc1XpXXwu0bFXXX.png", 
+		                "tce_rule_count": "1", 
+		                "link": "lenovo", 
+		                "title": "联想"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/TB1rVLKHpXXXXafaXXXSutbFXXX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "langsha", 
+		                "title": "浪莎"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/TB1FsffHXXXXXbaXVXXSutbFXXX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "newsmynsm", 
+		                "title": "纽曼车品"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/i4/T1w8nUXn4aXXXQXDnq-90-45.png", 
+		                "tce_rule_count": "1", 
+		                "link": "sast", 
+		                "title": "先科"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1NAeXFbBeXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "kailide", 
+		                "title": "凯立德"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1FPaZXCdeXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "toshibacc", 
+		                "title": "东芝"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1Z86AFXlbXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "jado", 
+		                "title": "捷渡车品"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1xKvZFj0cXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "garminxhwy", 
+		                "title": "星火伟业"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1x0imFvRjXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "flyaudio", 
+		                "title": "飞歌"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/T1utGfFf8gXXb1upjX.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "aimer", 
+		                "title": "爱慕"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/i1/T1J51oXXVjXXXXXXXX-90-45.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "steelmate", 
+		                "title": "铁将军"
+		            }, 
+		            {
+		                "img": "//img.alicdn.com/bao/uploaded/i1/T1_5.YXa0bXXXQXDnq-90-45.png", 
+		                "tce_rule_count": "1", 
+		                "link": "reebok", 
+		                "title": "锐步"
+		            }
+		        ], 
+		        "isTanx": "false", 
+		        "share": "false", 
+		        "id": "03014", 
+		        "open": "false", 
+		        "local": "false"
+		    }, 
+		    {
+		        "isAld": "true", 
+		        "alds": [
+		            {
+		                "img": "https://img.alicdn.com/tps/TB1qhWZNXXXXXb3XFXXXXXXXXXX-251-190.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "//ak47.tmall.com/campaign-3742-14.htm"
+		            }, 
+		            {
+		                "img": "https://img.alicdn.com/tps/TB1BBiZNXXXXXb4XFXXXXXXXXXX-251-190.jpg", 
+		                "tce_rule_count": "1", 
+		                "link": "//deejdyf.tmall.com"
+		            }
+		        ], 
+		        "isTanx": "false", 
+		        "share": "false", 
+		        "id": "201608180", 
+		        "open": "false", 
+		        "local": "false"
+		    }
+]
+
+data.forEach(function (item) {
+	let data = [];
+	let temp = item.alds;
+	temp.forEach(function (item2) {
+		let arr = [];
+		let img = item2.img;
+		arr.push(img)
+		return arr;
+	})
+	console.log(arr)
+})
+
+/*
+ * 遍历添加模拟的mock数据
+ */
+const productData = [];
+for (let i = 0; i < 100; i++) {
+	productData.push(Mock.mock({
+		id: i,
+		name: Mock.Random.cname(),
+		birthday: Mock.Random.date(),
+//		desc: 
+	}))
+}
+
+/*
+ * mock 响应数据
+ */
+Mock.mock('http://www.test.com', {
+	'id|+1': 0,//id初始值为1, 自动加1
+	'productDesc': data[0].desc,
+	'url': data[0].img 
+})
+
+console.log(productData)
